2d2f3d200f343c2b40e492cb2b5e3cb39293b84f
Albert Zedlitz <albert.zedlitz@gmail.com>
2016-07-07 15:15:35 +0200

    Comments
    Add license text for all modules

--- src/com/sap/douala/TDoualaView.java
+++ src/com/sap/douala/TDoualaView.java
@@ -5,6 +5,22 @@
 //   implementation of the functions 
 //   - "doUpdate"  for framework update events
 //   - "doRefresh" for action associated with "Refresh" button
+
+// Copyright (C) 2015  Albert Zedlitz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // ----------------------------------------------------------------------
 package com.sap.douala;
 
--- src/com/sap/douala/TTimeLaps.java
+++ src/com/sap/douala/TTimeLaps.java
@@ -4,6 +4,22 @@
 //   The TTimeLaps view collects trace events like GC, methods or exceptions.
 //   The table shows the tags "Timestamp", "Event" and "Info". The rest of the info is
 //   visible as tool tip or in the info status line.
+
+// Copyright (C) 2015  Albert Zedlitz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 // ------------------------------------------------------------------
 package com.sap.douala;
 
